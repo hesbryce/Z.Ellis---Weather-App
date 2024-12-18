fca8dcec3f9e150001f3ebc26e7aaca2e8ead0f2
Bryce <zbellis03@gmail.com>
2024-12-18 15:09:06 -0600

    Initial Commit

---
+++ Zachary_Ellis_Submission/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zachary_Ellis_Submission
+//
+//  Created by Bryce Ellis on 12/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zachary_Ellis_Submission/Zachary_Ellis_SubmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zachary_Ellis_SubmissionApp.swift
+//  Zachary_Ellis_Submission
+//
+//  Created by Bryce Ellis on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Zachary_Ellis_SubmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
